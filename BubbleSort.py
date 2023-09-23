dd585f0b1f81b684ae1e2329bc6a6e21999bda3a
5HAH33R <145150010+5HAH33R@users.noreply.github.com>
2023-09-23 10:48:22 +0500

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -6,7 +6,7 @@ def bubblesort(array):
             if array[j] > array[j + 1]:
                 temp = array[j]
                 array[j] = array[j + 1]
-                array[j] = temp
+                array[j + 1] = temp
 
 
 Array = [9, 5, 7, 3, 2, 5, 7, 8, 4, 2, 4, 6]
