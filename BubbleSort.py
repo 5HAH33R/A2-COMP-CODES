0a74f69b80b90bee488cbc3602498aab012b46d7
5HAH33R <145150010+5HAH33R@users.noreply.github.com>
2023-09-23 06:54:56 +0500

    Add files via upload

---
+++ BubbleSort.py
@@ -0,0 +1,15 @@
+
+def bubblesort(array):
+
+    for i in range(len(array)):
+        for j in range(len(array)-1):
+            if array[j] > array[j + 1]:
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j] = temp
+
+
+Array = [9, 5, 7, 3, 2, 5, 7, 8, 4, 2, 4, 6]
+
+print(bubblesort(Array))
+
